746203d1b92a86132989b6ab7d78b1a524e0e2e0
christine-png <142157013+christine-png@users.noreply.github.com>
2023-12-02 14:20:39 +0100

    ADD Day1
    --- Day 1: Trebuchet?! ---
    Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
    
    You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
    
    Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
    
    You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
    
    As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
    
    The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
    
    For example:
    
    1abc2
    pqr3stu8vwx
    a1b2c3d4e5f
    treb7uchet
    In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
    
    Consider your entire calibration document. What is the sum of all of the calibration values?
    
    Your puzzle answer was 54877.
    
    --- Part Two ---
    Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
    
    Equipped with this new information, you now need to find the real first and last digit on each line. For example:
    
    two1nine
    eightwothree
    abcone2threexyz
    xtwone3four
    4nineeightseven2
    zoneight234
    7pqrstsixteen
    In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
    
    What is the sum of all of the calibration values?

---
+++ Day1/Program.cs
@@ -0,0 +1,78 @@
+﻿
+
+using System.Numerics;
+
+String[] lines = File.ReadAllLines("data.txt");
+
+Dictionary<String, int> dictionary = new Dictionary<String, int>();
+dictionary.Add("one", 1);
+dictionary.Add("two", 2);
+dictionary.Add("three", 3);
+dictionary.Add("four", 4);
+dictionary.Add("five", 5);
+dictionary.Add("six", 6);
+dictionary.Add("seven", 7);
+dictionary.Add("eight", 8);
+dictionary.Add("nine", 9);
+
+int sum = 0;
+
+for (int i = 0; i < lines.Length; i++)
+{
+    bool firstFound = false;
+    string foundNumber = "";
+    int lastNumber = 0;
+
+    for (int j = 0; j < lines[i].Length; j++)
+    {
+
+        int num;
+
+        if (int.TryParse(lines[i][j] + "", out num))
+        {
+
+            if (firstFound == false)
+            {
+                foundNumber = lines[i][j] + "";
+                firstFound = true;
+            }
+            lastNumber = int.Parse(lines[i][j] + "");
+
+        }
+        else
+        {
+            foreach (var eintrag in dictionary)
+            {
+
+                if (j + eintrag.Key.Length <= lines[i].Length)
+                {
+
+                    if (lines[i].Substring(j, eintrag.Key.Length) == eintrag.Key)
+                    {
+                        if (firstFound == false)
+                        {
+                            foundNumber = eintrag.Value + "";
+                            firstFound = true;
+                        }
+                        lastNumber = int.Parse(eintrag.Value + "");
+                    }
+
+                }
+
+            }
+        }
+
+        
+
+
+    }
+
+
+    foundNumber = foundNumber + lastNumber;
+    sum += int.Parse(foundNumber);
+
+    
+
+}
+
+Console.WriteLine(sum);
